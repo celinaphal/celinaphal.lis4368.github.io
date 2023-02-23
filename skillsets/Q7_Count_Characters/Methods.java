a4b9c6d2d91191d38c4345f5c19de0076b2ce44f
cdp21d <cdp21d@fsu.edu>
2023-02-23 16:07:06 -0500

    a3

---
+++ skillsets/Q7_Count_Characters/Methods.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Methods
+{
+
+//Create method without returning any value
+public static void getRequirements()
+{
+    System.out.println("Developer: Celina Phal");
+    System.out.println("Program Requirements:");
+    System.out.println("1. Counts number and types of characters from user-entered string.");
+    System.out.println("2. Count: total, letters (upper-/lower-case), numbers, spaces, and other characters.");
+    System.out.println("Hint: Helpful methods: isLetter(), isDigit(), isSpaceChar(), and others.");
+
+    System.out.println();
+
+    System.out.println("Please enter string: Here is a Test String1!");
+    System.out.println("Total number of characters: 23");
+    System.out.println("Letter(s): 17");
+    System.out.println("Upper-case letter(s): 3");
+    System.out.println("Lower-case letter(s): 14");
+    System.out.println("Number(s): 1");
+    System.out.println("Space(s): 4");
+    System.out.println("Other character(s): 1");
+
+}
+}
